2e162c60031c7f725e2c26c1017524ae312c4a14
vik-kalsi <107356027+vik-kalsi@users.noreply.github.com>
2023-07-24 22:24:39 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,312 @@
+let btn1930 = document.querySelector("#btn1930");
+let winner1930 = document.querySelector("#winner1930");
+
+
+btn1930.addEventListener('click', ()=> {
+    if (winner1930.classList.contains("hidden")) {
+        winner1930.classList.remove("hidden");
+        btn1930.innerHTML = "Click to hide the winner of 1930";
+    } else {
+        winner1930.classList.add("hidden");
+        btn1930.innerHTML = "Click to view the winner of 1930";
+    }
+} );
+
+
+
+let btn1934 = document.querySelector("#btn1934");
+let winner1934 = document.querySelector("#winner1934");
+
+btn1934.addEventListener('click', ()=> {
+    if (winner1934.classList.contains("hidden")) {
+        winner1934.classList.remove("hidden");
+        btn1934.innerHTML = "Click to hide the winner of 1934";
+    } else {
+        winner1934.classList.add("hidden");
+        btn1934.innerHTML = "Click to view the winner of 1934";
+    }
+} );
+
+
+
+let btn1938 = document.querySelector("#btn1938");
+let winner1938 = document.querySelector("#winner1938");
+
+btn1938.addEventListener('click', ()=> {
+    if (winner1938.classList.contains("hidden")) {
+        winner1938.classList.remove("hidden");
+        btn1938.innerHTML = "Click to hide the winner of 1938";
+    } else {
+        winner1938.classList.add("hidden");
+        btn1938.innerHTML = "Click to view the winner of 1938";
+    }
+} );
+
+
+
+let btn1950 = document.querySelector("#btn1950");
+let winner1950 = document.querySelector("#winner1950");
+
+btn1950.addEventListener('click', ()=> {
+    if (winner1950.classList.contains("hidden")) {
+        winner1950.classList.remove("hidden");
+        btn1950.innerHTML = "Click to hide the winner of 1950";
+    } else {
+        winner1950.classList.add("hidden");
+        btn1950.innerHTML = "Click to view the winner of 1950";
+    }
+} );
+
+
+
+let btn1954 = document.querySelector("#btn1954");
+let winner1954 = document.querySelector("#winner1954");
+
+btn1954.addEventListener('click', ()=> {
+    if (winner1954.classList.contains("hidden")) {
+        winner1954.classList.remove("hidden");
+        btn1954.innerHTML = "Click to hide the winner of 1954";
+    } else {
+        winner1954.classList.add("hidden");
+        btn1954.innerHTML = "Click to view the winner of 1954";
+    }
+} );
+
+
+let btn1958 = document.querySelector("#btn1958");
+let winner1958 = document.querySelector("#winner1958");
+
+btn1958.addEventListener('click', ()=> {
+    if (winner1958.classList.contains("hidden")) {
+        winner1958.classList.remove("hidden");
+        btn1958.innerHTML = "Click to hide the winner of 1958";
+    } else {
+        winner1958.classList.add("hidden");
+        btn1958.innerHTML = "Click to view the winner of 1958";
+    }
+} );
+
+
+
+let btn1962 = document.querySelector("#btn1962");
+let winner1962 = document.querySelector("#winner1962");
+
+btn1962.addEventListener('click', ()=> {
+    if (winner1962.classList.contains("hidden")) {
+        winner1962.classList.remove("hidden");
+        btn1962.innerHTML = "Click to hide the winner of 1962";
+    } else {
+        winner1962.classList.add("hidden");
+        btn1962.innerHTML = "Click to view the winner of 1962";
+    }
+} );
+
+
+let btn1966 = document.querySelector("#btn1966");
+let winner1966 = document.querySelector("#winner1966");
+
+btn1966.addEventListener('click', ()=> {
+    if (winner1966.classList.contains("hidden")) {
+        winner1966.classList.remove("hidden");
+        btn1966.innerHTML = "Click to hide the winner of 1966";
+    } else {
+        winner1966.classList.add("hidden");
+        btn1966.innerHTML = "Click to view the winner of 1966";
+    }
+} );
+
+
+let btn1970 = document.querySelector("#btn1970");
+let winner1970 = document.querySelector("#winner1970");
+
+btn1970.addEventListener('click', ()=> {
+    if (winner1970.classList.contains("hidden")) {
+        winner1970.classList.remove("hidden");
+        btn1970.innerHTML = "Click to hide the winner of 1970";
+    } else {
+        winner1970.classList.add("hidden");
+        btn1970.innerHTML = "Click to view the winner of 1970";
+    }
+} );
+
+
+let btn1974 = document.querySelector("#btn1974");
+let winner1974 = document.querySelector("#winner1974");
+
+btn1974.addEventListener('click', ()=> {
+    if (winner1974.classList.contains("hidden")) {
+        winner1974.classList.remove("hidden");
+        btn1974.innerHTML = "Click to hide the winner of 1974";
+    } else {
+        winner1974.classList.add("hidden");
+        btn1974.innerHTML = "Click to view the winner of 1974";
+    }
+} );
+
+
+let btn1978 = document.querySelector("#btn1978");
+let winner1978 = document.querySelector("#winner1978");
+
+btn1978.addEventListener('click', ()=> {
+    if (winner1978.classList.contains("hidden")) {
+        winner1978.classList.remove("hidden");
+        btn1978.innerHTML = "Click to hide the winner of 1978";
+    } else {
+        winner1978.classList.add("hidden");
+        btn1978.innerHTML = "Click to view the winner of 1978";
+    }
+} );
+
+
+let btn1982 = document.querySelector("#btn1982");
+let winner1982 = document.querySelector("#winner1982");
+
+btn1982.addEventListener('click', ()=> {
+    if (winner1982.classList.contains("hidden")) {
+        winner1982.classList.remove("hidden");
+        btn1982.innerHTML = "Click to hide the winner of 1982";
+    } else {
+        winner1982.classList.add("hidden");
+        btn1982.innerHTML = "Click to view the winner of 1982";
+    }
+} );
+
+
+let btn1986 = document.querySelector("#btn1986");
+let winner1986 = document.querySelector("#winner1986");
+
+btn1986.addEventListener('click', ()=> {
+    if (winner1986.classList.contains("hidden")) {
+        winner1986.classList.remove("hidden");
+        btn1986.innerHTML = "Click to hide the winner of 1986";
+    } else {
+        winner1986.classList.add("hidden");
+        btn1986.innerHTML = "Click to view the winner of 1986";
+    }
+} );
+
+
+let btn1990 = document.querySelector("#btn1990");
+let winner1990 = document.querySelector("#winner1990");
+
+btn1990.addEventListener('click', ()=> {
+    if (winner1990.classList.contains("hidden")) {
+        winner1990.classList.remove("hidden");
+        btn1990.innerHTML = "Click to hide the winner of 1990";
+    } else {
+        winner1990.classList.add("hidden");
+        btn1990.innerHTML = "Click to view the winner of 1990";
+    }
+} );
+
+
+let btn1994 = document.querySelector("#btn1994");
+let winner1994 = document.querySelector("#winner1994");
+
+btn1994.addEventListener('click', ()=> {
+    if (winner1994.classList.contains("hidden")) {
+        winner1994.classList.remove("hidden");
+        btn1994.innerHTML = "Click to hide the winner of 1994";
+    } else {
+        winner1994.classList.add("hidden");
+        btn1994.innerHTML = "Click to view the winner of 1994";
+    }
+} );
+
+
+let btn1998 = document.querySelector("#btn1998");
+let winner1998 = document.querySelector("#winner1998");
+
+btn1998.addEventListener('click', ()=> {
+    if (winner1998.classList.contains("hidden")) {
+        winner1998.classList.remove("hidden");
+        btn1998.innerHTML = "Click to hide the winner of 1998";
+    } else {
+        winner1998.classList.add("hidden");
+        btn1998.innerHTML = "Click to view the winner of 1998";
+    }
+} );
+
+
+let btn2002 = document.querySelector("#btn2002");
+let winner2002 = document.querySelector("#winner2002");
+
+btn2002.addEventListener('click', ()=> {
+    if (winner2002.classList.contains("hidden")) {
+        winner2002.classList.remove("hidden");
+        btn2002.innerHTML = "Click to hide the winner of 2002";
+    } else {
+        winner2002.classList.add("hidden");
+        btn2002.innerHTML = "Click to view the winner of 2002";
+    }
+} );
+
+
+let btn2006 = document.querySelector("#btn2006");
+let winner2006 = document.querySelector("#winner2006");
+
+btn2006.addEventListener('click', ()=> {
+    if (winner2006.classList.contains("hidden")) {
+        winner2006.classList.remove("hidden");
+        btn2006.innerHTML = "Click to hide the winner of 2006";
+    } else {
+        winner2006.classList.add("hidden");
+        btn2006.innerHTML = "Click to view the winner of 2006";
+    }
+} );
+
+
+let btn2010 = document.querySelector("#btn2010");
+let winner2010 = document.querySelector("#winner2010");
+
+btn2010.addEventListener('click', ()=> {
+    if (winner2010.classList.contains("hidden")) {
+        winner2010.classList.remove("hidden");
+        btn2010.innerHTML = "Click to hide the winner of 2010";
+    } else {
+        winner2010.classList.add("hidden");
+        btn2010.innerHTML = "Click to view the winner of 2010";
+    }
+} );
+
+
+let btn2014 = document.querySelector("#btn2014");
+let winner2014 = document.querySelector("#winner2014");
+
+btn2014.addEventListener('click', ()=> {
+    if (winner2014.classList.contains("hidden")) {
+        winner2014.classList.remove("hidden");
+        btn2014.innerHTML = "Click to hide the winner of 2014";
+    } else {
+        winner2014.classList.add("hidden");
+        btn2014.innerHTML = "Click to view the winner of 2014";
+    }
+} );
+
+
+let btn2018 = document.querySelector("#btn2018");
+let winner2018 = document.querySelector("#winner2018");
+
+btn2018.addEventListener('click', ()=> {
+    if (winner2018.classList.contains("hidden")) {
+        winner2018.classList.remove("hidden");
+        btn2018.innerHTML = "Click to hide the winner of 2018";
+    } else {
+        winner2018.classList.add("hidden");
+        btn2018.innerHTML = "Click to view the winner of 2018";
+    }
+} );
+
+
+let btn2022 = document.querySelector("#btn2022");
+let winner2022 = document.querySelector("#winner2022");
+
+btn2022.addEventListener('click', ()=> {
+    if (winner2022.classList.contains("hidden")) {
+        winner2022.classList.remove("hidden");
+        btn2022.innerHTML = "Click to hide the winner of 2022";
+    } else {
+        winner2022.classList.add("hidden");
+        btn2022.innerHTML = "Click to view the winner of 2022";
+    }
+} );
